70a431acc1f9e6bb154305f45d9bce4928e65b0e
Alejandro Duran <alejandroduran1730@gmail.com>
2025-03-18 17:26:06 -0600

    Added base project with JWT dependencies

---
+++ src/main/java/paperwork/PaperworkApplication.java
@@ -0,0 +1,13 @@
+package paperwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/paperwork/PaperworkApplicationTests.java
@@ -0,0 +1,13 @@
+package paperwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9e42de4f7778731f5a42f1fdc22366b134898c52
Alejandro Duran <alejandroduran1730@gmail.com>
2025-03-19 10:25:12 -0600

    Added security folder as well as JwtUtils

---
+++ src/main/java/security/JwtUtils.java
@@ -0,0 +1,43 @@
+package security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expirationMs}")
+    private int expirationMs;
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            // Log exception
+            System.out.println("Token validation error");
+        }
+        return false;
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret)
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+}
